775c260116b5c97dd93cc27e7c05d2f628cdb86b
DinoStark <15190304050@163.com>
2025-03-07 19:27:16 +0800

    Initial commit

---
+++ src/main/java/stark/reshaper/magicinsight/MagicInsightMain.java
@@ -0,0 +1,13 @@
+package stark.reshaper.magicinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicInsightMain
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MagicInsightMain.class, args);
+    }
+}
---
+++ src/main/java/stark/reshaper/magicinsight/domain/entities/DomainBase.java
@@ -0,0 +1,40 @@
+package stark.reshaper.magicinsight.domain.entities;
+
+import lombok.Data;
+import stark.coderaider.fluentschema.commons.annotations.AutoIncrement;
+import stark.coderaider.fluentschema.commons.annotations.NotMapped;
+import stark.coderaider.fluentschema.commons.annotations.PrimaryKey;
+
+import java.util.Date;
+
+@Data
+@NotMapped
+public abstract class DomainBase
+{
+    /**
+     * ID of the record.
+     */
+    @PrimaryKey
+    @AutoIncrement
+    private long id;
+
+    /**
+     * ID of the creator of the record.
+     */
+    private long creatorId;
+
+    /**
+     * Creation time of the record.
+     */
+    private Date creationTime;
+
+    /**
+     * ID of the user who modifies the record.
+     */
+    private long modifierId;
+
+    /**
+     * Last modification time of the record.
+     */
+    private Date modificationTime;
+}
